fd438d44913d768b8822abb26a25e0981f62d26b
Евгений Сергеев <alexevgenii@mail.ru>
2021-09-28 15:52:32 +0400

    Add Laba n1

---
+++ MonoRelsR/MonoRelsR/Direction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoRelsR
+{
+    //Перечисление для указания направления движения
+    public enum Direction
+    {
+        //Вверх
+        Up,
+        //Влево
+        Left,
+        //Вправо
+        Right,
+        //Вниз
+        Down
+    }
+}
