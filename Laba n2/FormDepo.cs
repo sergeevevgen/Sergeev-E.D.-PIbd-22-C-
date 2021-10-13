34ce695226652964792a52d33732668b0aa71c2b
Евгений Сергеев <alexevgenii@mail.ru>
2021-10-13 23:16:13 +0400

    Fix bug with didnt disappeare lines

--- Laba n2/FormDepo.cs
+++ Laba n2/FormDepo.cs
@@ -48,6 +48,9 @@ namespace Laba_n2
             {
                 listBoxLevels.SelectedIndex = index;
             }
+            else if(listBoxLevels.Items.Count == 0)
+                pictureBoxDepo.Image = null;
+                
         }
 
         /// <summary>
@@ -179,7 +182,7 @@ namespace Laba_n2
         private void buttonDeleteDepo_Click(object sender, EventArgs e)
         {
             //Удаляет парковку, основываясь на тексте в текстБоксе
-            if (listBoxLevels.SelectedIndex > -1)
+            /*if (listBoxLevels.SelectedIndex > -1)
             {
                 if (MessageBox.Show($"Удалить парковку {listBoxLevels.SelectedItem.ToString()}?",
                     "Удаление", MessageBoxButtons.YesNo,
@@ -188,9 +191,9 @@ namespace Laba_n2
                     depoCollection.DelDepo(textBoxNewLevelName.Text);
                     ReloadLevels();
                 }
-            }
+            }*/
             //Удаляет выбранную парковку
-            /*if (listBoxLevels.SelectedIndex > -1)
+            if (listBoxLevels.SelectedIndex > -1)
             {
                 if (MessageBox.Show($"Удалить парковку {listBoxLevels.SelectedItem.ToString()}?",
                     "Удаление", MessageBoxButtons.YesNo,
@@ -199,7 +202,7 @@ namespace Laba_n2
                     depoCollection.DelDepo(listBoxLevels.SelectedItem.ToString());
                     ReloadLevels();
                 }
-            }*/
+            }
 
             //Удаление по имени из текстБокса
             /*if (MessageBox.Show($"Удалить парковку {textBoxNewLevelName.Text}?",
